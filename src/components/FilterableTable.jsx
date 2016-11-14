1a3035e92fe4f4c3131a8493a4b017344a1220bf
rcdmk <rcdmk@dtavares.com>
2016-11-14 21:05:49 -0200

    Extracted the filterable table component and applied to the widgets list too

---
+++ src/components/FilterableTable.jsx
@@ -0,0 +1,64 @@
+/**
+ * Filterable table widget definition
+ */
+import React from 'react';
+
+export default class FilterableTable extends React.Component {
+    constructor(props) {
+        super(props);
+
+        var p = props || this.props || {};
+
+        // initial state
+        this.state = this.state || {};
+        this.state.list = p.list || [];
+
+        // needed to bind the current context
+        this.filterRecords = this.filterRecords.bind(this);
+    }
+
+    filterRecords() {
+        var searchString = this.refs.filterInput.value;
+        var filteredList = this.props.list.filter((item, i) => item.name.toLowerCase().indexOf(searchString) >= 0);
+
+        this.setState({ list: filteredList });
+    }
+
+    render() {
+        return (
+            <div className="widget">
+                <div className="widget-header">{this.props.title}
+                    <div className="pull-right"><input type="text" className="form-control input-sm" ref="filterInput" onChange={this.filterRecords} /></div>
+                </div>
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th className="text-center">ID</th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                // if
+                                this.state.list.length ?
+                                    this.state.list.map(function (item, i) {
+                                        return (
+                                            <tr key={item.id}>
+                                                <td className="text-center">{item.id}</td>
+                                                <td>{item.name}</td>
+                                            </tr> 
+                                        );
+                                    })
+                                : // else
+                                    <tr>
+                                        <td colSpan="2">No records found!</td>
+                                    </tr>
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import HeaderBar from './HeaderBar';
 import DashboardBox from './DashboardBox';
+import FilterableTable from './FilterableTable'
 
 export default class IndexPage extends React.Component {
     constructor(props) {
@@ -13,18 +14,6 @@ export default class IndexPage extends React.Component {
 
         // initial state
         this.state = this.state || {};
-        this.state.users = p.users || [];
-        this.state.widgets = p.widgets || [];
-
-        // needed to bind the current context
-        this.filterUsers = this.filterUsers.bind(this);
-    }
-
-    filterUsers() {
-        var searchString = this.refs.filterUsersInput.value;
-        var filteredUsers = this.props.users.filter((u, i) => u.name.toLowerCase().indexOf(searchString) >= 0);
-
-        this.setState({ users: filteredUsers });
     }
 
     render() {
@@ -40,75 +29,11 @@ export default class IndexPage extends React.Component {
                 <div className="row">
 
                     <div className="col-lg-6">
-                        <div className="widget">
-                            <div className="widget-header">Users
-                                <div className="pull-right"><input type="text" className="form-control input-sm" ref="filterUsersInput" onChange={this.filterUsers} /></div>
-                            </div>
-                            <div className="table-responsive">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th className="text-center">ID</th>
-                                            <th>Name</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            // if
-                                            this.state.users.length ?
-                                                this.state.users.map(function (user, i) {
-                                                    return (
-                                                        <tr key={user.id}>
-                                                            <td className="text-center">{user.id}</td>
-                                                            <td>{user.name}</td>
-                                                        </tr> 
-                                                    );
-                                                })
-                                            : // else
-                                                <tr>
-                                                    <td colSpan="2">No records found!</td>
-                                                </tr>
-                                        }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                        <FilterableTable title="Users" list={this.props.users} />
                     </div>
 
                     <div className="col-lg-6">
-                        <div className="widget">
-                            <div className="widget-header">Widgets
-                                <div className="pull-right"><input type="text" className="form-control input-sm" /></div>
-                            </div>
-                            <div className="table-responsive">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th className="text-center">ID</th>
-                                            <th>Name</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            // if
-                                            this.state.widgets.length ?
-                                                this.state.widgets.map(function (widget, i) {
-                                                    return (
-                                                        <tr key={widget.id}>
-                                                            <td className="text-center">{widget.id}</td>
-                                                            <td>{widget.name}</td>
-                                                        </tr> 
-                                                    );
-                                                })
-                                            : // else
-                                                <tr>
-                                                    <td colSpan="2">No records found!</td>
-                                                </tr>
-                                        }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                        <FilterableTable title="Widgets" list={this.props.widgets} />
                     </div>
                 </div>
             </div>
