ec131b797913b147f874ccd57dc2d2b51c634655
rcdmk <rcdmk@dtavares.com>
2016-11-15 20:57:10 -0200

    Marked current page link as active on the sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@
  * Dashboard (index) page definition
  */
 import React from 'react';
-import { Link } from 'react-router';
+import { IndexLink, Link } from 'react-router';
 
 export default class Layout extends React.Component {
     render() {
@@ -10,11 +10,11 @@ export default class Layout extends React.Component {
             <div id="page-wrapper" className="open">
                 <div id="sidebar-wrapper">
                     <ul className="sidebar">
-                        <li className="sidebar-main"><Link to="/">Dashboard <span className="menu-icon fa fa-tachometer"></span></Link></li>
+                        <li className="sidebar-main"><IndexLink to="/">Dashboard <span className="menu-icon fa fa-tachometer"></span></IndexLink></li>
                         <li className="sidebar-title"><span>NAVIGATION</span></li>
-                        <li className="sidebar-list"><Link to="/">Dashboard <span className="menu-icon fa fa-tachometer"></span></Link></li>
-                        <li className="sidebar-list"><Link to="/users">Users <span className="menu-icon fa fa-users"></span></Link></li>
-                        <li className="sidebar-list"><Link to="/widgets">Widgets <span className="menu-icon fa fa-cubes"></span></Link></li>
+                        <li className="sidebar-list"><IndexLink to="/" key="index" activeClassName="active">Dashboard <span className="menu-icon fa fa-tachometer"></span></IndexLink></li>
+                        <li className="sidebar-list"><Link to="/users" activeClassName="active">Users <span className="menu-icon fa fa-users"></span></Link></li>
+                        <li className="sidebar-list"><Link to="/widgets" activeClassName="active">Widgets <span className="menu-icon fa fa-cubes"></span></Link></li>
                     </ul>
                     <div className="sidebar-footer col-xs-12"><a href="#" target="_blank">&copy; 2015 Red Ventures</a></div>
                 </div>
