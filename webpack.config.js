48c50179146856797596b60e59a45ada088ee126
rcdmk <rcdmk@dtavares.com>
2016-11-15 19:47:36 -0200

    Changed some file extensions to reflect JSX syntax

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'app.js'),
+    entry: path.join(__dirname, 'src', 'app.jsx'),
     output: {
         path: path.join(__dirname, 'public', 'js'),
         filename: 'bundle.js'
