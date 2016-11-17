fd68d7398790a4df45eea7bea9e6d033b2c9a801
rcdmk <rcdmk@dtavares.com>
2016-11-17 00:12:09 -0200

    Implemented get user by id API route

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -8,6 +8,21 @@ function UsersController() {
   return {
     listUsers: function listUsers(req, res) {
       res.send(users);
+    },
+    getUserById: function getUserById(req, res) {
+      if (!req.params.id || isNaN(parseInt(req.params.id))) {
+        return res.sendStatus(400);
+      }
+      
+      const id = req.params.id;
+
+      const user = users.filter((u, i) => u.id == id)[0];
+
+      if (!user) {
+        res.sendStatus(404);
+      } else {
+        res.send(user);
+      }
     }
   };
 }
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ var usersController = require('../controllers/api/usersController');
 
 /* GET users listing. */
 router.get('/users', usersController.listUsers);
+router.get('/users/:id', usersController.getUserById);
 
 
 module.exports = router;
